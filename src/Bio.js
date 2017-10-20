24bacc08411acb9943b81493d48aea42a26fe6eb
Keith Laidlaw <keithlaidlaw@gmail.com>
2017-10-20 15:11:45 -0700

    Add new files

---
+++ src/Bio.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'; 
+
+export default class Bio extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img src={this.props.portrait} alt={this.props.name} />
+          <h1 className="App-title">{this.props.name}</h1>
+        </header>
+        <p className="App-intro">
+          <ul>
+            {this.makeBullets()}
+          </ul>
+        </p>
+      </div>
+    );
+  }
+
+  makeBullets() {
+    return this.props.bullets.map(bullet => <li>{bullet}</li>);
+  }
+}
+
+/*
+
+import React, { Component } from 'react'; 
+
+export default class Bio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bullets_visible: true,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <header className="App-header" onClick={() => this.handleClick()}>
+          <img src={this.props.portrait} alt={this.props.name} />
+          <h1 className="App-title">{this.props.name}</h1>
+        </header>
+        <div className={this.makeBulletClasses()}>
+          <ul>
+            {this.makeBullets()}
+          </ul>
+        </p>
+      </div>
+    );
+  }
+
+  makeBullets() {
+    return this.props.bullets.map(bullet, index) => {
+      return <li key={index}>{bullet}</li>;
+    });
+  }
+
+  handleClick() {
+    this.setState({
+      bullets_visible = !this.state.bullets_visible,
+    });
+  }
+  makeBulletClasses() {
+    let classes = "App-intro";
+    if (!this.state.bullets_visible) {
+      classes = " App-intro-hidden";
+    }
+    return classes;
+  }
+}
+
+*/
